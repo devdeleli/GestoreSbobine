0eefcf9af191979d3c852a4ef67bf25a2753f959
MECS <delrossoelia@gmail.com>
2023-05-08 19:44:13 +0200

    mmm

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from googleapiclient.http import MediaFileUpload
 from tkinter import filedialog
+from uploader.UploadDrive import upload_pdf_to_drive
 import os
 
 def center_window(window):
@@ -33,34 +34,7 @@ creds = service_account.Credentials.from_service_account_file(creds_path, scopes
 
 
 # Define function to handle file upload to Google Drive
-def upload_pdf_to_drive():
-    # Prompt user to select file to upload
-    filepath = filedialog.askopenfilename(initialdir=os.getcwd(), title="Select a PDF file",
-                                          filetypes=(("PDF files", "*.pdf"), ("all files", "*.*")))
-    if not filepath:
-        return
 
-    # Set the folder ID of the destination folder on Google Drive
-    folder_id = '1QQiyUMkPm3Z-kwBS69zM6DV4svvdWHGU'
-
-    # Create the Google Drive API client object
-    service = build('drive', 'v3', credentials=creds)
-
-    # Set the metadata for the new file
-    file_metadata = {
-        'name': os.path.basename(filepath),
-        'parents': [folder_id],
-        'mimeType': 'application/pdf'
-    }
-
-    # Upload the file to Google Drive
-    try:
-        media = MediaFileUpload(filepath, mimetype='application/pdf')
-        file = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
-        print(f"File ID: {file.get('id')} - '{file.get('name')}' has been uploaded to Google Drive.")
-    except HttpError as error:
-        print(f"An error occurred: {error}")
-        file = None
 
 
 # Create the splash screen window
@@ -101,18 +75,7 @@ def main():
     btn_upload = Button(root, text="Upload PDF to Google Drive", command=upload_pdf_to_drive)
     btn_upload.pack(pady=20)
 
-    img1 = ImageTk.PhotoImage(Image.open("./Media/button1.png"))
-    button1 = Button(frame, image=img1, text="Button 1", compound="bottom", command=button1_click, bd=0,
-                     highlightthickness=0)
-    button1.image = img1
-    button1.pack(side="left", padx=10, pady=10)
 
-    # create the second button
-    img2 = ImageTk.PhotoImage(Image.open("./Media/button2.png"))
-    button2 = Button(frame, image=img2, text="Button 2", compound="bottom", command=button2_click, bd=0,
-                     highlightthickness=0)
-    button2.image = img2
-    button2.pack(side="left", padx=10, pady=10)
 
 
 # Set interval
---
+++ uploader/UploadDrive.py
@@ -0,0 +1,37 @@
+from tkinter import *
+from PIL import Image, ImageTk
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from googleapiclient.http import MediaFileUpload
+from tkinter import filedialog
+import os
+
+def upload_pdf_to_drive(folder_id:str):
+    # Prompt user to select file to upload
+    filepath = filedialog.askopenfilename(initialdir=os.getcwd(), title="Select a PDF file",
+                                          filetypes=(("PDF files", "*.pdf"), ("all files", "*.*")))
+    if not filepath:
+        return
+
+    # Set the folder ID of the destination folder on Google Drive
+    #folder_id = '1QQiyUMkPm3Z-kwBS69zM6DV4svvdWHGU'
+
+    # Create the Google Drive API client object
+    service = build('drive', 'v3', credentials=creds)
+
+    # Set the metadata for the new file
+    file_metadata = {
+        'name': os.path.basename(filepath),
+        'parents': [folder_id],
+        'mimeType': 'application/pdf'
+    }
+
+    # Upload the file to Google Drive
+    try:
+        media = MediaFileUpload(filepath, mimetype='application/pdf')
+        file = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+        print(f"File ID: {file.get('id')} - '{file.get('name')}' has been uploaded to Google Drive.")
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+        file = None
